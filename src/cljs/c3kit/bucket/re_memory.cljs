bfeb1ee5033b7b4f2b0ea3e2ce9219735a6acf6c
Connor Kilgore <connor.kilgore@gmail.com>
2025-07-03 16:04:02 -0700

    updated some functions to use cursors

--- src/cljs/c3kit/bucket/re_memory.cljs
+++ src/cljs/c3kit/bucket/re_memory.cljs
@@ -1,24 +1,46 @@
 (ns c3kit.bucket.re-memory
   (:refer-clojure :rename {find core-file count core-count reduce core-reduce})
-  (:require [c3kit.apron.legend :as legend]
+  (:require [c3kit.apron.corec :as ccc]
+            [c3kit.apron.legend :as legend]
             [c3kit.bucket.api :as api]
             [c3kit.bucket.memory :refer [MemoryDB] :as memory]
             [reagent.core :as r]))
 
+;; db api -----------------------------------
+(defn entity
+  "kind (optional) in addition to returning nil when kinds don't match,
+  will allow coercion of the id to the right id type for the kind."
+  ([db id]
+   (cond
+     (nil? id) nil
+     (map? id) @(r/cursor (.-store db) [:all (:id id)])
+     :else @(r/cursor (.-store db) [:all id])))
+  ([db kind id]
+   (if (nil? kind)
+     (entity db id)
+     (cond
+       (nil? id) nil
+       (map? id) @(r/cursor (.-store db) [kind (api/-coerced-id @(.-legend db) kind (:id id))])
+       :else @(r/cursor (.-store db) [kind (api/-coerced-id @(.-legend db) kind id)])))))
+
+(defn- do-find [db kind options]
+  (memory/ensure-schema! @(.-legend db) kind)
+  (let [es (or (vals @(r/cursor (.-store db) [kind])) [])]
+    (->> (ccc/find-by es (:where options))
+         (api/-apply-drop-take options))))
 
 (deftype ReMemoryDB [legend store]
   api/DB
   (-clear [this] (memory/clear this))
   (close [_this] (comment "Nothing to do here"))
-  (-count [this kind options] (core-count (memory/do-find this kind options)))
+  (-count [this kind options] (core-count (do-find this kind options)))
   (-delete-all [this kind] (memory/delete-all this kind))
-  (-entity [this kind id] (memory/entity this kind id))
-  (-find [this kind options] (memory/do-find this kind options))
-  (-reduce [this kind f init options] (core-reduce f init (memory/do-find this kind options)))
+  (-entity [this kind id] (entity this kind id))
+  (-find [this kind options] (do-find this kind options))
+  (-reduce [this kind f init options] (core-reduce f init (do-find this kind options)))
   (-tx [this entity] (memory/tx this entity))
   (-tx* [this entities] (memory/tx* this entities)))
 
 (defmethod api/-create-impl :re-memory [config schemas]
   (let [store (or (:store config) (r/atom {}))]
     (ReMemoryDB. (atom (legend/build schemas)) store)))
-
